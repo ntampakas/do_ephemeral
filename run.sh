999f1d705f8a751b05bc6477bf46198459a61956
Nick Tampakas <nick@gmx.co.uk>
2024-02-21 15:48:20 +0200

    Refactor schedule function

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ set -eux
 EXIT_CODE=0
 
 if [ "$1" = "schedule" ]; then
-  doctl compute droplet list --format "Name,ID,Status" | grep off | awk '$1 ~ /^ntampakas-/ {print $2}' | xargs  doctl compute droplet delete -f
+  droplets=$(doctl compute droplet list --format "Name,ID,Status" | grep off | awk '$1 ~ /^ntampakas-/ {print $2}')
+  [ -z "$droplets" ] || doctl compute droplet delete $droplets -f
   exit 0
 fi
 
